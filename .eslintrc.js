56e871d267e341f88381266055a73a4530c14857
leejs1030 <leejs1030@korea.ac.kr>
2022-05-07 22:16:39 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'implicit-arrow-linebreak': 'off',
+    curly: 'off',
+    'nonblock-statement-body-position': 'off',
+    'function-paren-newline': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+};
