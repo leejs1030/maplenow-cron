c6fc7372a6a1c8c7cadaa63715c68460928e85e8
leejs1030 <leejs1030@korea.ac.kr>
2022-05-09 03:02:20 +0900

    maplenow를 업데이트 하여 코너 케이스 대응

--- src/libs/html/prettierHtml.tsx
+++ src/libs/html/prettierHtml.tsx
@@ -1,5 +1,4 @@
 import { mapletype, Probs } from 'maplenow-tool';
-import axios from 'axios';
 import BasePage from 'libs/html/basePage';
 import ReactDOMServer from 'react-dom/server';
 import prettier from 'prettier';
@@ -20,20 +19,7 @@ const makePrettier = async ({
   const TableInfo = paragraphs;
   const TableItem = await Probs.getBaseProbsWithUuid(pageUuid, subPageUuid, paragraphs);
   const TableTitle = TableInfo.filter((e) => e.autoTable).map((value) => value.autoTable.header);
-  const UA =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36 Edg/101.0.1210.39';
-  let CollectionInfo: string;
-  try {
-    CollectionInfo = (
-      await axios.get(description as string, {
-        headers: {
-          'User-Agent': UA,
-        },
-      })
-    ).data;
-  } catch (err) {
-    CollectionInfo = TableInfo[0].autoTable.header;
-  }
+  const CollectionInfo: string = description as string;
 
   const x = (
     <BasePage
