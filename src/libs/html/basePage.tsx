06c0033466e251d316bf71121a66305ae7931598
leejs1030 <leejs1030@korea.ac.kr>
2022-05-09 00:56:19 +0900

    테이블 정보를 더 잘 보여주도록 수정

--- src/libs/html/basePage.tsx
+++ src/libs/html/basePage.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { mapletype } from 'maplenow-tool';
+import GenerateTable from 'libs/html/generateTable';
 
-const BasePage = ({ SelectedPage }: { SelectedPage: NextPage }) => {
+const BasePage: NextPage<{
+  TableItem: mapletype.AutoTableItem[][][];
+  TableTitle: string[];
+  TableInfo: { uuid: string; autoTable: mapletype.AutoTable }[];
+}> = ({ TableItem, TableTitle, TableInfo }) => {
   return (
     <html>
       <head>
         <meta charSet="UTF-8" />
+        <style>{'table, th, td { border: 1px solid; }'}</style>
       </head>
       <body>
-        <SelectedPage />
+        <GenerateTable TableItem={TableItem} TableTitle={TableTitle} TableInfo={TableInfo} />
       </body>
     </html>
   );
--- src/libs/html/generateTable.tsx
+++ src/libs/html/generateTable.tsx
@@ -3,21 +3,48 @@ import { mapletype } from 'maplenow-tool';
 import { colMapper } from '../../constants';
 import UseInnerHtml from 'libs/html/useInnerHtml';
 
+const GenerateBody: NextPage<{ TableItem: mapletype.AutoTableItem[][][]; colArr: string[] }> = ({
+  TableItem,
+  colArr,
+}) => {
+  return (
+    <div>
+      <tbody>
+        {TableItem.map((d2arr) => {
+          // 근본적으로 3차원 배열이기에 코드가 복잡.
+          return d2arr.map((arr) => {
+            return arr.map((tableItem: any) => {
+              // 여기서부터 tableItem이 개별 객체가 됨.
+              return (
+                <tr className={'check'}>
+                  {colArr.map((value) => {
+                    // 실제로 사용될 col은 모두 colArr에 저장되어 있음.
+                    return <td>{tableItem[value]}</td>; // tableItem으로부터 실제로 사용할 col을 뽑아오기.
+                  })}
+                </tr>
+              );
+            });
+          });
+        })}
+      </tbody>
+    </div>
+  );
+};
+
 const generateTable = (
   title: string,
   t: mapletype.AutoTable,
   x: mapletype.AutoTableItem[][][],
   colArr: string[],
-  GenerateBody: NextPage<{ TableItem: mapletype.AutoTableItem[][][]; colArr: string[] }>,
 ) => {
   return (
-    <div>
+    <div className={'innerbefore'}>
       <UseInnerHtml str={title} />
       <table>
         <thead>
           <tr>
             {t.contentColActive ? <th>{t.contentColAlias}</th> : null}
-            {t.trialresultColActive ? <th>{t.trialresultColAlias}</th> : null}
+            {t.trialColActive ? <th>{t.trialColAlias}</th> : null}
             {t.probtableColActive ? <th>{t.probtableColAlias}</th> : null}
             {t.trialresultColActive ? <th>{t.trialresultColAlias}</th> : null}
             {t.countColActive ? <th>{t.countColAlias}</th> : null}
@@ -25,35 +52,14 @@ const generateTable = (
             {t.realProbColActive ? <th>{t.realProbColAlias}</th> : null}
           </tr>
         </thead>
-        <GenerateBody TableItem={x} colArr={colArr} />
+        <span className={'afds'}>
+          <GenerateBody TableItem={x} colArr={colArr} />
+        </span>
       </table>
+      <br />
+      <br />
+      <br />
     </div>
-  );
-};
-
-const GenerateBody: NextPage<{ TableItem: mapletype.AutoTableItem[][][]; colArr: string[] }> = ({
-  TableItem,
-  colArr,
-}) => {
-  return (
-    <tbody>
-      {TableItem.map((d2arr) => {
-        // 근본적으로 3차원 배열이기에 코드가 복잡.
-        return d2arr.map((arr) => {
-          return arr.map((tableItem: any) => {
-            // 여기서부터 tableItem이 개별 객체가 됨.
-            return (
-              <tr>
-                {colArr.map((value) => {
-                  // 실제로 사용될 col은 모두 colArr에 저장되어 있음.
-                  return tableItem[value]; // tableItem으로부터 실제로 사용할 col을 뽑아오기.
-                })}
-              </tr>
-            );
-          });
-        });
-      })}
-    </tbody>
   );
 };
 
@@ -69,14 +75,17 @@ const GenerateTable: NextPage<{
         colArr.push([]);
         const t = value.autoTable;
         if (t.contentColActive) colArr[index].push(colMapper.contentColActive);
-        if (t.trialresultColActive) colArr[index].push(colMapper.contentColActive);
+        if (t.trialColActive) colArr[index].push(colMapper.trialColActive);
         if (t.probtableColActive) colArr[index].push(colMapper.probtableColActive);
         if (t.trialresultColActive) colArr[index].push(colMapper.trialresultColActive);
         if (t.countColActive) colArr[index].push(colMapper.countColActive);
         if (t.probColActive) colArr[index].push(colMapper.probColActive);
         if (t.realProbColActive) colArr[index].push(colMapper.realProbColActive);
-        return generateTable(TableTitle[index], t, TableItem, colArr[index], GenerateBody);
+        return generateTable(TableTitle[index], t, TableItem, colArr[index]);
       })}
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
