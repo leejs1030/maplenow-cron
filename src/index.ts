fb4871363cdf9319505f52b88a893b714ee9433c
leejs1030 <leejs1030@korea.ac.kr>
2022-05-08 16:54:27 +0900

    dependency 조정

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
 import { CronJob } from 'cron';
-import cheerio from 'cheerio';
-import { generateAllHtml } from './generateHtml';
-
-const $ = cheerio.load('');
-$('head').append('<meta charset="utf-8">\n');
 
 const job = new CronJob('*/10 * * * * *', () => {
-  generateAllHtml($);
+  console.log('1');
 });
 
 job.start();
