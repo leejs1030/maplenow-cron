76b3ed885abcdc279dc21d55e5955ed3d7cc2398
leejs1030 <leejs1030@korea.ac.kr>
2022-05-08 22:54:33 +0900

    스켈레톤 코드 변경(각각 html 생기는 방식으로 수정)

--- src/generateHtml/basePage.tsx
+++ src/generateHtml/basePage.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { mapletype } from 'maplenow-tool';
-import { SelectPage } from './selectPage';
-import { allPageEnum } from 'custom-type';
+import { NextPage } from 'next';
 
-const Hello = ({
-  pageName,
-  subPageName,
-}: {
-  pageName: mapletype.pageEnum;
-  subPageName: allPageEnum;
-}) => {
+const BasePage = ({ SelectedPage }: { SelectedPage: NextPage }) => {
   return (
     <html>
       <head>
         <meta charSet="UTF-8" />
       </head>
       <body>
-        <SelectPage subPageNumber={subPageName} pageNumber={pageName} />
+        <SelectedPage />
       </body>
     </html>
   );
 };
 
-export default Hello;
+export default BasePage;
