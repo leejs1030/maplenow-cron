37c5aaaaf903e2b2195cdf880133b860e79f1f8b
leejs1030 <leejs1030@korea.ac.kr>
2022-05-09 02:02:11 +0900

    beauty 페이지 저장 구현

--- src/generateHtml/beauty/index.ts
+++ src/generateHtml/beauty/index.ts
@@ -1,3 +1,45 @@
-const generateBeautyPage = async () => {};
+import { Paragraphs } from 'maplenow-tool';
+import writeHtml from 'libs/html/writeHtml';
+
+const basedir = 'htmls/beauty';
+
+const royalHairPage = async () => {
+  const { pageUuid, subPageUuid, paragraphs, description } =
+    await Paragraphs.Beauty.getRoyalHairParagraphList(false);
+  const CurrentPageTitle = '로얄 헤어 쿠폰';
+  const directory = `${basedir}/royal-hair.html`;
+  writeHtml({ pageUuid, subPageUuid, paragraphs, description, CurrentPageTitle, directory });
+};
+
+const royalFacePage = async () => {
+  const { pageUuid, subPageUuid, paragraphs, description } =
+    await Paragraphs.Beauty.getRoyalHairParagraphList(false);
+  const CurrentPageTitle = '로얄 성형 쿠폰';
+  const directory = `${basedir}/royal-face.html`;
+  writeHtml({ pageUuid, subPageUuid, paragraphs, description, CurrentPageTitle, directory });
+};
+
+const royalHairPageChange = async () => {
+  const { pageUuid, subPageUuid, paragraphs, description } =
+    await Paragraphs.Beauty.getRoyalHairParagraphList(true);
+  const CurrentPageTitle = '체인지 로얄 헤어 쿠폰';
+  const directory = `${basedir}/royal-hair-change.html`;
+  writeHtml({ pageUuid, subPageUuid, paragraphs, description, CurrentPageTitle, directory });
+};
+
+const royalFacePageChange = async () => {
+  const { pageUuid, subPageUuid, paragraphs, description } =
+    await Paragraphs.Beauty.getRoyalHairParagraphList(true);
+  const CurrentPageTitle = '체인지 로얄 성형 쿠폰';
+  const directory = `${basedir}/royal-face-change.html`;
+  writeHtml({ pageUuid, subPageUuid, paragraphs, description, CurrentPageTitle, directory });
+};
+
+const generateBeautyPage = async () => {
+  royalHairPage();
+  royalFacePage();
+  royalHairPageChange();
+  royalFacePageChange();
+};
 
 export default generateBeautyPage;
