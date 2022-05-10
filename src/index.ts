2ea8867bb5fb8e9122f1b3d71b0fac548feb9129
leejs1030 <leejs1030@korea.ac.kr>
2022-05-10 23:27:43 +0900

    dependency, devedependency 변경

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { CronJob } from 'cron';
 import generateHtml from './generateHtml';
-
-const job = new CronJob('*/10 * * * * *', async () => {
-  console.log(new Date().toLocaleString());
-  await generateHtml();
-});
-
-// job.start();
 
 generateHtml();
