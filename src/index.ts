2dd36fdecde70c4001321c2496d44fe1b222031c
leejs1030 <leejs1030@korea.ac.kr>
2022-05-11 02:20:49 +0900

    nexon 추가

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import generateHtml from './generateHtml';
 import { CronJob } from 'cron';
 import axios from 'axios';
 
-axios
-  .get('https://github.com/')
-  .then(
-    (data) => console.log(data.data),
-    (reason) => console.log(reason || reason?.data),
-  )
-  .catch((err) => console.log(err || err?.data));
+// axios
+//   .get('https://github.com/')
+//   .then(
+//     (data) => console.log(data.data),
+//     (reason) => console.log(reason || reason?.data),
+//   )
+//   .catch((err) => console.log(err || err?.data));
 axios
   .get('https://now.nexon.com/service/maplestory?page=0ea22a8a-31ff-4e41-8c10-9ea6d90dd244')
   .then(
