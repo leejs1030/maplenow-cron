b9dfe2bf44af3a38dbd6a24656cdf3cab0dd2eec
leejs1030 <leejs1030@korea.ac.kr>
2022-05-11 01:58:14 +0900

    axios 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import generateHtml from './generateHtml';
 import { CronJob } from 'cron';
+import axios from 'axios';
 
-if (process.env.CRON_REQUIRED) {
-  const job = new CronJob('* */5 * * * *', async () => {
-    console.log(new Date().toLocaleString());
-    await generateHtml();
-  });
-  job.start();
-}
-generateHtml()
+axios
+  .get('https://github.com/')
   .then(
-    () => console.log('succes!!!'),
-    (errReason) => console.error(errReason),
+    (data) => console.log(data.data),
+    (reason) => console.log(reason || reason?.data),
   )
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err || err?.data));
+
+// if (process.env.CRON_REQUIRED) {
+//   const job = new CronJob('* */5 * * * *', async () => {
+//     console.log(new Date().toLocaleString());
+//     await generateHtml();
+//   });
+//   job.start();
+// }
+// generateHtml()
+//   .then(
+//     () => console.log('succes!!!'),
+//     (errReason) => console.error(errReason),
+//   )
+//   .catch((err) => console.log(err));
